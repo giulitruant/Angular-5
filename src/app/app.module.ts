44b4b5886e622399e4d980dcde3070a5acee0c38
Giuliano Truant <giuliano.truant@neoris.com>
2018-08-06 17:48:20 -0300

    hero - No finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,63 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
-import { map } from 'rxjs/operators';
-import { HttpClient } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { MueblesComponent } from './Mueble/muebles.component';
-import { OficinaComponent } from './Oficina/oficina.component';
-import { TiposMaderasComponent } from './Maderas/maderas.component';
-import { DormitorioComponent } from './Dormitorio/dormitorio.component';
-import { CocinaComponent } from './Cocina/cocina.component';
-import { LivingComponent } from './Living/living.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { MessagesComponent } from './messages/messages.component';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MueblesComponent,
-    OficinaComponent,
-    TiposMaderasComponent,
-    DormitorioComponent,
-    CocinaComponent,
-    LivingComponent
-  ],
   imports: [
-    
     BrowserModule,
     FormsModule,
-    HttpModule,    
-    RouterModule.forRoot([
-      {
-        path: 'Muebles',
-        component: MueblesComponent
-      },
-      {
-        path: 'Cocina',
-        component: CocinaComponent
-      },
-      {
-        path: 'Dormitorio',
-        component: DormitorioComponent
-      },
-      {
-        path: 'Living',
-        component: LivingComponent
-      },
-      {
-        path: 'Maderas',
-        component: TiposMaderasComponent
-      },
-      {
-        path: 'Oficina',
-        component: OficinaComponent
-      }
-    ])    
+    AppRoutingModule
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
